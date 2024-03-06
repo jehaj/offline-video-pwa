663fc8a61fa769a0a0597da3253824b4cd566fb3
Nikolaj J-K <kagenogmig@gmail.com>
2024-03-06 17:11:51 +0100

    service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const cacheName = "offline-v1";
+const contentToCache = [
+    "/index.html",
+    "/video.svg",
+    "/script.js"
+];
+self.addEventListener("install", event => {
+    console.log("[Service Worker] Install");
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(contentToCache);
+        })(),
+    );
+});
+
+self.addEventListener("fetch", event => {
+    console.log(`[Service Worker] Fetched resource ${event.request.url}`);
+    event.respondWith(
+        (async () => {
+            const response = await caches.match(event.request);
+            console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
+            return response || await fetch(event.request);
+        })(),
+    );
+});
